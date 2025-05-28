f0fb14882c4f6e0ea4a3001591ab0ad16083d442
nagaratokuma <nagaratokuma@gmail.com>
2025-05-29 06:58:40 +0900

    baseURLの誤りを修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/youtube-sync-viewer/',
+  base: '/sync-youtube-viewer/',
   plugins: [react()],
 })
